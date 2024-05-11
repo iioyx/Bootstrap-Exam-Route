cb53417ffc6fdebd1cd318659cc0e8a1427af3c8
Yousef ibrahim <108943127+iioyx@users.noreply.github.com>
2024-05-11 12:52:20 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#element", {
+  strings: ["Developer", "Designer", "Larry Daniels"],
+  typeSpeed: 100,
+  loop: true,
+  loopCount: Infinity,
+  backDelay: 2000,
+  backSpeed: 10,
+  smartBackspace: false,
+});
